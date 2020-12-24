9ddca95685142e42aa7da451a6514d59d0beaa64
sh2MAN <simons2007@yandex.ru>
2020-12-24 12:14:30 +0400

    DJ App:recipe,users register ModelAdmin

--- foodgram/recipes/admin.py
+++ foodgram/recipes/admin.py
@@ -1,15 +1,26 @@
 from django.contrib import admin
 
-from .models import Ingredient, Recipe
+from .models import Ingredient, Recipe, RecipeIngredients
 
 
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
     list_display = ('title', 'author')
     list_filter = ('author', 'title', 'tags')
+    ordering = ('-pub_date',)
 
 
 class IngredientAdmin(admin.ModelAdmin):
     model = Ingredient
     list_display = ('title', 'units')
     list_filter = ('title',)
+
+
+class RecipeIngredientsAdmin(admin.ModelAdmin):
+    model = RecipeIngredients
+    list_display = ('recipe__title', 'ingredient__title')
+
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(RecipeIngredients, RecipeIngredientsAdmin)
--- foodgram/users/admin.py
+++ foodgram/users/admin.py
@@ -1,13 +1,25 @@
 from django.contrib import admin
 
-from .models import User
+from .models import Favorite, Subscription, User
+
+
+class FavoriteAdmin(admin.ModelAdmin):
+    model = Favorite
+    list_display = ('user__username', 'recipe__title')
+
+
+class SubscriptionAdmin(admin.ModelAdmin):
+    model = Subscription
+    list_display = ('user__username', 'author__username')
 
 
 class UserAdmin(admin.ModelAdmin):
     model = User
-    list_display = (
-        'email', 'login', 'is_staff', 'is_active'
-    )
-    list_filter = (
-        'email', 'login'
-    )
+    list_display = ('email', 'username', 'is_staff', 'is_active')
+    list_filter = ('email', 'username')
+
+
+admin.site.unregister(User)
+admin.site.register(Favorite, FavoriteAdmin)
+admin.site.register(Subscription, SubscriptionAdmin)
+admin.site.register(User, UserAdmin)
